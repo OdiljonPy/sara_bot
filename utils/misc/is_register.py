7c5e17baf1cf3940735092d16957a867fcc17b47
sattorov <sattorov.ahu@gmail.com>
2023-12-29 00:33:53 +0500

    change is_register function

--- utils/misc/is_register.py
+++ utils/misc/is_register.py
@@ -1,4 +1,8 @@
-# from data.config import X_API_KEY, DOMAIN
+import requests
+from data.config import DOMAIN
+
+
+# from data.config import X_API_KEY
 
 # headers = {
 #     'X-API-KEY': X_API_KEY
@@ -6,3 +10,4 @@
 
 def is_register(user_id: int = None):
     return True
+    # return requests.post(url=f"{DOMAIN}/user_tg/check_user/", params={"user_id": user_id}).json().get("ok")
